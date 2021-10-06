06a9e63958c7f413aeed9b292308b0a24bf74ba7
DraimCiDo <danilaorlov4@gmail.com>
2021-10-06 14:31:29 +0300

    [0.0.2] Create package

---
+++ src/main/java/me/draimgoose/draimshop/plugin/DraimShop.java
@@ -0,0 +1,17 @@
+package me.draimgoose.draimshop.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DraimShop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
