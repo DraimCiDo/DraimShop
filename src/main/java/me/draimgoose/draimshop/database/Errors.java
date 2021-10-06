f287243ad496ed65e04e377c8980160ae7a12e4b
DraimCiDo <danilaorlov4@gmail.com>
2021-10-06 17:07:08 +0300

    [0.2.8-2] Errors

--- src/main/java/me/draimgoose/draimshop/database/Errors.java
+++ src/main/java/me/draimgoose/draimshop/database/Errors.java
@@ -1,4 +1,11 @@
 package me.draimgoose.draimshop.database;
 
 public class Errors {
+    public static String sqlConnectionExecute() {
+        return "Не удалось выполнить инструкцию SQL: ";
+    }
+
+    public static String sqlConnectionClose() {
+        return "Не удалось закрыть соединение SQL: ";
+    }
 }
