7dd8bd8a9ab7c0ef2dd660f08803aef887468782
DraimCiDo <danilaorlov4@gmail.com>
2021-10-06 17:40:55 +0300

    [0.3.3] ExternalPluginsSupport

--- src/main/java/me/draimgoose/draimshop/plugin/ExternalPluginsSupport.java
+++ src/main/java/me/draimgoose/draimshop/plugin/ExternalPluginsSupport.java
@@ -1,13 +1,20 @@
 package me.draimgoose.draimshop.plugin;
 
+import dev.lone.itemsadder.api.CustomStack;
 import me.draimgoose.draimshop.plugin.DraimShopLogger.LVL;
+import me.draimgoose.draimshop.shop.briefcase.BCCreator;
+import me.draimgoose.draimshop.shop.vm.VMCreator;
 import me.draimgoose.draimshop.utils.MsgUtils.MSG;
 import org.apache.commons.lang.WordUtils;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import me.pikamug.localelib.LocaleLib;
 import me.pikamug.localelib.LocaleManager;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+import java.util.Set;
 
 public class ExternalPluginsSupport {
     private DraimShop pl;
@@ -53,5 +60,49 @@ public class ExternalPluginsSupport {
                 }
             }.runTask(this.pl);
         }
+    }
+
+    public HashMap<Integer, ItemStack> getModelDataToShopMapping() {
+        if (!has("ItemsAdder"))
+            return null;
+        HashMap<Integer, ItemStack> map = new HashMap<>();
+
+        Set<String> vm = this.pl.getConfig().getConfigurationSection("vending-machine").getKeys(false);
+        for (String shop : vm) {
+            Integer customModelData = this.pl.getConfig().getInt("vending-machine." + shop + ".model-data");
+            ItemStack item = CustomStack.getInstance("draimshop:" + shop + "_vending_machine").getItemStack();
+            map.put(customModelData, item);
+        }
+        Set<String> nb = this.pl.getConfig().getConfigurationSection("briefcase").getKeys(false);
+        for (String shop : nb) {
+            Integer customModelData = this.pl.getConfig().getInt("briefcase." + shop + ".model-data");
+            ItemStack item = CustomStack.getInstance("draimshop:" + shop + "_briefcase").getItemStack();
+            map.put(customModelData, item);
+        }
+
+        Integer defaultVM = this.pl.getConfig().getInt("defaults.vending-machine");
+        ItemStack defaultVMItem = CustomStack.getInstance("draimshop:default_vending_machine").getItemStack();
+        map.put(defaultVM, defaultVMItem);
+        Integer defaultBriefcase = this.pl.getConfig().getInt("defaults.briefcase");
+        ItemStack defaultBriefcaseItem = CustomStack.getInstance("draimshop:default_briefcase").getItemStack();
+        map.put(defaultBriefcase, defaultBriefcaseItem);
+
+        return map;
+    }
+
+    public Boolean isDefaultModel(ItemStack item) {
+        if (!has("ItemsAdder"))
+            return null;
+        String id = CustomStack.byItemStack(item).getNamespacedID();
+        return id.contains("default");
+    }
+
+    public Object getShopCreator(ItemStack item) {
+        if (!has("ItemsAdder"))
+            return null;
+        String id = CustomStack.byItemStack(item).getNamespacedID();
+        if (id.contains("vending_machine"))
+            return new VMCreator();
+        return new BCCreator();
     }
 }
