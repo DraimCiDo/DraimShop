b9f19b89bdf642c9b86cef11320f73c8170b23c4
DraimCiDo <danilaorlov4@gmail.com>
2021-10-06 15:12:18 +0300

    [0.1.7] + в чат если любишь статичные цвета

--- src/main/java/me/draimgoose/draimshop/plugin/DraimShopLogger.java
+++ src/main/java/me/draimgoose/draimshop/plugin/DraimShopLogger.java
@@ -1,4 +1,30 @@
 package me.draimgoose.draimshop.plugin;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
+
 public class DraimShopLogger {
+
+    public enum LVL {
+        FAIL(ChatColor.RED), INFO(ChatColor.AQUA), WARN(ChatColor.YELLOW), SUCCESS(ChatColor.GREEN);
+
+        private ChatColor color;
+
+        LVL(ChatColor color) {
+            this.color = color;
+        }
+
+        @Override
+        public String toString() {
+            return this.color.toString();
+        }
+    }
+
+    private static ConsoleCommandSender getLogger() {
+        return DraimShop.getPlugin().getServer().getConsoleSender();
+    }
+
+    public static void sendMSG(String msg, LVL type) {
+        getLogger().sendMessage(type + "(DraimShop)" + msg);
+    }
 }
