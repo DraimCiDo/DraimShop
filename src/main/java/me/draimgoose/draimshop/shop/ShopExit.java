65bfa052c6b015fad94dcaa99602f6467e8f6a52
DraimCiDo <danilaorlov4@gmail.com>
2021-10-06 18:04:10 +0300

    [0.4.0] ShopExit

--- src/main/java/me/draimgoose/draimshop/shop/ShopExit.java
+++ src/main/java/me/draimgoose/draimshop/shop/ShopExit.java
@@ -1,4 +1,16 @@
 package me.draimgoose.draimshop.shop;
 
-public class ShopExit {
+import me.draimgoose.draimshop.player.PlayerState;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+
+public class ShopExit implements Listener {
+    @EventHandler
+    public void closeShop(InventoryCloseEvent evt) {
+        if (!((Player) evt.getPlayer()).isConversing()) {
+            PlayerState.getPlayerState((Player) evt.getPlayer()).clearShopInteractions();
+        }
+    }
 }
