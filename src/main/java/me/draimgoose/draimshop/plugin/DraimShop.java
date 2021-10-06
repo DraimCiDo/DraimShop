71088fe2eade0833b2e58b3e4d0cd894e5889485
DraimCiDo <danilaorlov4@gmail.com>
2021-10-06 14:55:45 +0300

    [0.1.3]

--- src/main/java/me/draimgoose/draimshop/plugin/DraimShop.java
+++ src/main/java/me/draimgoose/draimshop/plugin/DraimShop.java
@@ -3,15 +3,20 @@ package me.draimgoose.draimshop.plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DraimShop extends JavaPlugin {
+    private static DraimShop plInst;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        plInst = this;
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
+    }
+
+    public static DraimShop getPlugin() {
+        return plInst;
     }
 }
