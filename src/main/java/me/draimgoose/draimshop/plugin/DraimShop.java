08441c1c4220afd6f504deb233e1dc2d6617e002
DraimCiDo <danilaorlov4@gmail.com>
2021-10-06 15:17:11 +0300

    [0.1.8] Полноценный подхват Vault

--- src/main/java/me/draimgoose/draimshop/plugin/DraimShop.java
+++ src/main/java/me/draimgoose/draimshop/plugin/DraimShop.java
@@ -1,7 +1,9 @@
 package me.draimgoose.draimshop.plugin;
 
 import me.draimgoose.draimshop.database.DB;
+import me.draimgoose.draimshop.plugin.DraimShopLogger.LVL;
 import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DraimShop extends JavaPlugin {
@@ -12,12 +14,32 @@ public final class DraimShop extends JavaPlugin {
     @Override
     public void onEnable() {
         plInst = this;
+        if (!setUpEco()) {
+            DraimShopLogger.sendMSG("Опа, а где Vault сука? Выключаю твою тарахтелку..", LVL.FAIL);
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        } else {
+            DraimShopLogger.sendMSG("Схватил за яички Vault", LVL.SUCCESS);
+        }
 
     }
 
     @Override
     public void onDisable() {
+    }
 
+    private boolean setUpEco() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        } else {
+            RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+            if (rsp == null) {
+                return false;
+            } else {
+                eco = rsp.getProvider();
+                return eco != null;
+            }
+        }
     }
 
     public static DraimShop getPlugin() {
