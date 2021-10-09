695ea8f3c3e661936330fd9c7bce9bff10d7ec60
DraimCiDo <danilaorlov4@gmail.com>
2021-10-09 19:39:30 +0300

    [0.5.9] ProtocolLibHandler

--- src/main/java/me/draimgoose/draimshop/plugin/ProtocolLibHandler.java
+++ src/main/java/me/draimgoose/draimshop/plugin/ProtocolLibHandler.java
@@ -1,4 +1,27 @@
 package me.draimgoose.draimshop.plugin;
 
-public class ProtocolLibHandler {
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.EnumWrappers.PlayerDigType;
+import org.bukkit.event.block.BlockDamageEvent;
+import org.bukkit.plugin.Plugin;
+
+public class ProtocolLibHandler extends PacketAdapter {
+    private BlockDamageEvent evt;
+
+    public ProtocolLibHandler(Plugin plugin, PacketType type, BlockDamageEvent evt) {
+        super(plugin, type);
+        this.evt = evt;
+    }
+
+    @Override
+    public void onPacketReceiving(PacketEvent e) {
+        PacketContainer packet = e.getPacket();
+        PlayerDigType digType = packet.getPlayerDigTypes().getValues().get(0);
+        if (digType.name().equals("ABORT_DESTROY_BLOCK")) {
+            this.evt.setCancelled(true);
+        }
+    }
 }
