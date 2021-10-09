c1bd5f32980ff29ef92128bf456e06a733babef9
DraimCiDo <danilaorlov4@gmail.com>
2021-10-09 20:03:50 +0300

    [1.0.2] Выдача владельца магазина

--- src/main/java/me/draimgoose/draimshop/shop/GetShopOwner.java
+++ src/main/java/me/draimgoose/draimshop/shop/GetShopOwner.java
@@ -1,4 +1,32 @@
 package me.draimgoose.draimshop.shop;
 
-public class GetShopOwner {
+import me.draimgoose.draimshop.plugin.DSComd;
+import me.draimgoose.draimshop.utils.LangUtils;
+import me.draimgoose.draimshop.utils.ShopUtils;
+import org.bukkit.block.Block;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Player;
+
+public class GetShopOwner extends DSComd {
+    public GetShopOwner(CommandSender sender) {
+        super(sender, null);
+    }
+
+    @Override
+    public boolean exec() {
+        if (this.sender instanceof Player) {
+            Player player = (Player) this.sender;
+            Block targetBlock = player.getTargetBlockExact(5);
+            ArmorStand armorStand = ShopUtils.getArmorStand(targetBlock);
+            if (armorStand != null) {
+                player.sendMessage(
+                        String.format(LangUtils.getString("shop-owner"), ShopUtils.getOwner(armorStand).getName()));
+            } else {
+                player.sendMessage(LangUtils.getString("invalid-target"));
+
+            }
+        }
+        return true;
+    }
 }
