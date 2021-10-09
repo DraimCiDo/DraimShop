d4966d5067db11458f86eebb15e6242dbba694c7
DraimCiDo <danilaorlov4@gmail.com>
2021-10-09 20:06:48 +0300

    [1.0.2] Создание команды /ds reload

--- src/main/java/me/draimgoose/draimshop/plugin/Reload.java
+++ src/main/java/me/draimgoose/draimshop/plugin/Reload.java
@@ -1,4 +1,24 @@
 package me.draimgoose.draimshop.plugin;
 
-public class Reload {
+import me.draimgoose.draimshop.utils.LangUtils;
+import org.bukkit.command.CommandSender;
+
+public class Reload extends DSComd {
+
+    public Reload(CommandSender sender) {
+        super(sender, null);
+    }
+
+    @Override
+    public boolean exec() {
+        if (!sender.hasPermission("customshop.reload")) {
+            sender.sendMessage(LangUtils.getString("command-no-perms"));
+        } else {
+            DraimShop.getPlugin().reloadConfig();
+            LangUtils.loadLangConfig();
+            sender.sendMessage("§7(§6DraimShop§7) §aКонфигурация успешно перезагружена.");
+        }
+        return true;
+    }
+
 }
