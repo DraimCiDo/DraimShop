3314a244cb73e1f81c1874009860b64832296b63
DraimCiDo <danilaorlov4@gmail.com>
2021-10-09 19:52:09 +0300

    [0.6.5] PlayerJoin

--- src/main/java/me/draimgoose/draimshop/player/PlayerJoin.java
+++ src/main/java/me/draimgoose/draimshop/player/PlayerJoin.java
@@ -1,4 +1,22 @@
 package me.draimgoose.draimshop.player;
 
-public class PlayerJoin {
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+import me.draimgoose.draimshop.plugin.DraimShop;
+import me.draimgoose.draimshop.utils.MsgUtils;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoin implements Listener {
+    @EventHandler
+    public void onJoin(PlayerJoinEvent evt) {
+        CompletableFuture.runAsync(() -> {
+            DraimShop plugin = DraimShop.getPlugin();
+            List<MsgUtils.MSG> messages = plugin.getDB()
+                    .getMessages(evt.getPlayer().getUniqueId().toString());
+            messages.forEach(e -> plugin.support().sendMSG(evt.getPlayer(), e));
+        });
+    }
 }
