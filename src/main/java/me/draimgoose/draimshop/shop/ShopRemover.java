7d05d41da98047fc5805fd283ea7930b30dfe9e1
DraimCiDo <danilaorlov4@gmail.com>
2021-10-09 18:43:06 +0300

    [0.4.7] ShopRemover

--- src/main/java/me/draimgoose/draimshop/shop/ShopRemover.java
+++ src/main/java/me/draimgoose/draimshop/shop/ShopRemover.java
@@ -1,4 +1,12 @@
 package me.draimgoose.draimshop.shop;
 
-public class ShopRemover {
+import java.util.UUID;
+import org.bukkit.block.Block;
+import org.bukkit.entity.ArmorStand;
+
+public abstract class ShopRemover {
+    protected Block targetBlock;
+    protected ArmorStand armorStand;
+    protected UUID ownerUUID;
+    public abstract UUID removeShop(boolean dropItems);
 }
