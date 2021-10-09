1f7dcf2b3fcb3d99f82f06540258c79223aa7e9d
DraimCiDo <danilaorlov4@gmail.com>
2021-10-09 19:49:09 +0300

    [0.6.3] PlayerTeleport

--- src/main/java/me/draimgoose/draimshop/player/PlayerTeleport.java
+++ src/main/java/me/draimgoose/draimshop/player/PlayerTeleport.java
@@ -1,4 +1,13 @@
 package me.draimgoose.draimshop.player;
 
-public class PlayerTeleport {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+
+public class PlayerTeleport implements Listener {
+    @EventHandler
+    public void onTeleport(PlayerTeleportEvent evt) {
+        PlayerState.getPlayerState(evt.getPlayer()).clearShopInteractions();
+    }
 }
