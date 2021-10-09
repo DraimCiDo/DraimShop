a5f99b6b60cba32f12d99a9c411e5d7bb2f57435
DraimCiDo <danilaorlov4@gmail.com>
2021-10-09 19:52:01 +0300

    [0.6.5] PlayerLeave

--- src/main/java/me/draimgoose/draimshop/player/PlayerLeave.java
+++ src/main/java/me/draimgoose/draimshop/player/PlayerLeave.java
@@ -1,4 +1,19 @@
 package me.draimgoose.draimshop.player;
 
-public class PlayerLeave {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerKickEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+
+public class PlayerLeave implements Listener {
+    @EventHandler
+    public void playerKick(PlayerKickEvent evt) {
+        PlayerState.getPlayerState(evt.getPlayer()).clearShopInteractions();
+    }
+
+    @EventHandler
+    public void playerLeave(PlayerQuitEvent evt) {
+        PlayerState.getPlayerState(evt.getPlayer()).clearShopInteractions();
+    }
 }
